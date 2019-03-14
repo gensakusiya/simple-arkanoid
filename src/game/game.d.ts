62f57bd8c9c8ae0cae78ac52b6e5b11d9953c11a
Artyom Prikhodko <homoogenecus@gmail.com>
2019-03-14 13:10:35 +0300

    update module interface

--- src/game/game.d.ts
+++ src/game/game.d.ts
@@ -3,7 +3,31 @@ export interface IGame {
     stop(): void;
 }
 
+export interface ILevel {
+    level: number;
+    score: number;
+    lives: number;
+    speed: number;
+
+    next(): void;
+}
+
+interface IGameObject {
+    x: number;
+    y: number;
+    color: string;
+
+    draw(ctx: CanvasRenderingContext2D): void;
+    updatePosition(position: Position): void;
+}
+
 export type Position = {
     x: number;
     y: number;
+}
+
+export type Color = {
+    PADDLE: string;
+    BALL: string;
+    BRICK: string;
 }
