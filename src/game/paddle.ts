aa9ac492a907ccf019b25205e209c19a75a668d6
Artyom Prikhodko <homoogenecus@gmail.com>
2019-03-14 13:11:12 +0300

    created main paddle state

---
+++ src/game/paddle.ts
@@ -0,0 +1,29 @@
+import {IGameObject, Position} from "./game";
+
+class Paddle implements IGameObject {
+    static WIDTH: number = 80;
+    static HEIGHT: number = 10;
+
+    x: number = 0;
+    y: number = 0;
+    color: string = '';
+
+    constructor(x: number = 0, y: number = 0, color: string = '') {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    draw(ctx: CanvasRenderingContext2D): void {
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, Paddle.WIDTH, Paddle.HEIGHT);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    updatePosition(position: Position): void {
+    }
+}
+
+export default Paddle;
